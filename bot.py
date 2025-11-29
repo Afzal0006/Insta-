58a2b4e2dacbfe789b66f7e892297a436ded2d8c
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-11-29 21:06:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,22 +2,15 @@ from pyrogram import Client, filters
 
 app = Client(
     "userbot",
-    api_id=24526311,          
-    api_hash="717d5df262e474f88d86c537a787c98d",  
+    api_id=24526311,
+    api_hash="717d5df262e474f88d86c537a787c98d"
 )
 
-@app.on_message(filters.command("dice") & filters.me)
-async def dice_handler(client, message):
-    # dice send
+@app.on_message(filters.me & filters.regex(r"^🎲$"))
+async def send_dice(client, message):
     msg = await message.reply_dice()
-
-    # dice ka result
     result = msg.dice.value
-
-    # heroku logs me print
     print(f"🎲 Dice Result: {result}")
-
-    # optional: apne saved message me bhejna ho toh
     await client.send_message("me", f"🎲 Dice Result: {result}")
 
 app.run()
